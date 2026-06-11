2d74583a1611aae882b946e85a9859d45c4bb129
Stagiaire DG 2 <stagedg2@mairie.crosne>
2026-06-11 17:06:07 +0200

    feat(ui): add Bruno Crampé account, update user profile ui and move source budgetaire to settings

--- tarification-api/src/main/java/fr/mairie/tarification_api/DataInitializer.java
+++ tarification-api/src/main/java/fr/mairie/tarification_api/DataInitializer.java
@@ -27,5 +27,15 @@ public class DataInitializer implements CommandLineRunner {
         admin.setActif(true);
         repository.save(admin);
         System.out.println("✅ Compte 'admin' prêt et synchronisé (Mot de passe: admin)");
+
+        // Compte pour le DGS Bruno Crampé
+        Utilisateur bruno = repository.findByUsername("crosne.bcrampé").orElse(new Utilisateur());
+        bruno.setUsername("crosne.bcrampé");
+        bruno.setPassword("Fin30!67#crsn"); 
+        bruno.setNom("Bruno Crampé");
+        bruno.setRole("DGS (Finances)");
+        bruno.setActif(true);
+        repository.save(bruno);
+        System.out.println("✅ Compte 'crosne.bcrampé' prêt (Mot de passe: Fin30!67#crsn)");
     }
 }
